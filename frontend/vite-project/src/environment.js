f608ef0855062e5f9d55ee222e51508248e92149
Sarvpriya Batar <batarsarvpriya@gmail.com>
2026-07-18 12:46:00 +0530

    Deployment Frontend

--- frontend/vite-project/src/environment.js
+++ frontend/vite-project/src/environment.js
@@ -1,3 +1,8 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://apnacollegebackend.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
