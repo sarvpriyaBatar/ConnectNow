e12d04f376fab54eb6567340bf7e532d5a7e38ba
Sarvpriya Batar <batarsarvpriya@gmail.com>
2026-07-18 12:02:32 +0530

    Ready for Deployment

---
+++ backend/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import Schema from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,}
+});
+ const User = mongoose.model("User", userSchema);
+ export { User };
